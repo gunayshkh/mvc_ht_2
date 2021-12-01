99bfa68567650b0b9d77aca68c529cf64e4b2b55
gunayshkh <88051220+gunayshkh@users.noreply.github.com>
2021-12-01 21:41:53 +0400

    Add files via upload

---
+++ MVC2View/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC2View.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            if (false)
+            {
+
+            return View();
+            }
+            return Json(new { name = "Hello", surname = "World" });
+        }
+        
+    }
+}
---
+++ MVC2View/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC2View.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            if (id==null)
+            {
+                ViewBag.data = "Id is null";
+                return View();
+
+            }
+            return Json(new { Name = "Jason", Surnam="Statham", Age = 50 });
+            
+        }
+    }
+}
